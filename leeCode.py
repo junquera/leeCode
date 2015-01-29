de0a5f98e18b04a5e32111da3832ca1a016f46cd
Javier Junquera Sánchez <javier.junquera.sanchez@gmail.com>
2015-01-29 14:45:09 +0100

    Nuevas opciones

--- leeCode.py
+++ leeCode.py
@@ -1,14 +1,19 @@
 import sys
 
+sH = False
+
 def uso():
-	print "USO\n\tleeHex.py [ARCHIVO]"
+	print "USO\n\tleeHex.py [ARCHIVO] [OPCION]\n\nOPCION\n\tsH\n\t\tMostrar solo el codigo hexadecimal (para poder copiarlo)"
 
 def hexArchivo(archivo):
+
+	global sH
 
 	contador = 0
 	x = open(archivo, 'rb')
 
-	y = "0x{0:04x}".format(contador) + "\t"
+	if not sH:
+		y = "0x{0:04x}".format(contador) + "\t"
 
 	for i in x.read():
 		y += "{0:02x}".format(ord(i))
@@ -16,11 +21,16 @@ def hexArchivo(archivo):
 
 		if contador%16 == 0:
 			print y 
-			y = "0x{0:04x}".format(contador) + "\t"
+			if not sH:
+				y = "0x{0:04x}".format(contador) + "\t"
 		else:
 			y+=' '
 
 	print y
+
+def soloHex():
+	global sH
+	sH = True
 
 def main(argv=None):
 	if argv is None:
@@ -30,8 +40,20 @@ def main(argv=None):
 		print sys.argv
 		hexArchivo(argv[1])
 	else:
-		uso()
-		
+		if(len(argv) == 3):
+			opcion = {'sH': soloHex(),}
+
+			try:
+				opcion[argv[2]]
+				hexArchivo(argv[1])
+			except:
+				print "Opcion '{0}' no valida\n".format(argv[2])
+				print sys.exc_info()[0]
+				uso()
+
+		else:
+			uso()
+
 	return 0
 
 if __name__ == '__main__':
